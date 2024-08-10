47c421862bf804b60464f01e13ea87ad78af7213
Askar Mamytov <askar.mamytov201@gmail.com>
2024-08-10 11:36:50 -0500

    updates routes and route tables for both vpcs

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# cloud provider
 provider "aws" {
   region = "us-east-1"
 }
 
+# vpc comprised of 2 subnets (public and private)
 resource "aws_vpc" "vpc_a" {
   cidr_block = "10.100.0.0/16"
   tags = {
@@ -9,6 +11,7 @@ resource "aws_vpc" "vpc_a" {
   }
 }
 
+# vpc comprised of 1 private subnet
 resource "aws_vpc" "vpc_b" {
   cidr_block = "10.200.0.0/16"
   tags = {
@@ -16,7 +19,7 @@ resource "aws_vpc" "vpc_b" {
   }
 }
 
-# create 2 subnets for vpc-a
+# create 2 subnets for vpc-a (public and private)
 resource "aws_subnet" "public-subnet-vpc-a" {
   vpc_id                  = aws_vpc.vpc_a.id
   cidr_block              = "10.100.10.0/24"
@@ -43,14 +46,15 @@ resource "aws_subnet" "private-subnet-vpc_b" {
   }
 }
 
+# create internet gateway for vpc-a public subnet
 resource "aws_internet_gateway" "igw_public_sub" {
   vpc_id = aws_vpc.vpc_a.id
-
   tags = {
     Name = "igw_public_sub"
   }
 }
 
+# create route table for vpc-a public subnet
 resource "aws_route_table" "public_route_vpc_a" {
   vpc_id = aws_vpc.vpc_a.id
   tags = {
@@ -58,20 +62,46 @@ resource "aws_route_table" "public_route_vpc_a" {
   }
 }
 
+# create route for vpc-a public subnet
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_route_vpc_a.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw_public_sub.id
 }
 
+# associate route table with public subnet
 resource "aws_route_table_association" "pub_assoc" {
   route_table_id = aws_route_table.public_route_vpc_a.id
   subnet_id      = aws_subnet.public-subnet-vpc-a.id
 }
 
-# create instances on each vpc
-# -- configure security groups
-# vpc-a public subnet ingress and egress rules: Allow ssh traffic on port 22 outbound on all traffic
+# create route table for vpc-a private subnet
+resource "aws_route_table" "private_route_vpc_a" {
+  vpc_id = aws_vpc.vpc_a.id
+  tags = {
+    Name = "private_route_vpc_a"
+  }
+}
+
+# associate route table with private subnet
+resource "aws_route_table_association" "priv_assoc" {
+  route_table_id = aws_route_table.private_route_vpc_a.id
+  subnet_id      = aws_subnet.private-subnet-vpc-a.id
+}
+
+# create route table for vpc-b private subnet
+resource "aws_route_table" "private_route_vpc_b" {
+  vpc_id = aws_vpc.vpc_b.id
+  tags = {
+    Name = "private_route_vpc_b"
+  }
+}
+
+# associate route table with private subnet
+resource "aws_route_table_association" "priv_assoc_vpc_b" {
+  route_table_id = aws_route_table.private_route_vpc_b.id
+  subnet_id      = aws_subnet.private-subnet-vpc_b.id
+}
 
 # Create security group for public VPC-A subnet
 resource "aws_security_group" "sg_public_subnet_vpc_a" {
@@ -166,6 +196,7 @@ resource "aws_instance" "vpc_a_public_ec2" {
 
 }
 
+# create ec2 for vpc-a private subnet
 resource "aws_instance" "vpc_a_private_ec2" {
   ami             = "ami-0ae8f15ae66fe8cda" # Replace with the appropriate AMI ID for your region
   instance_type   = "t2.micro"
@@ -175,9 +206,32 @@ resource "aws_instance" "vpc_a_private_ec2" {
   tags = {
     Name = "private_ec2_vpc_a"
   }
-
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
-
 }
+
+# create ec2 for vpc-b private subnet
+resource "aws_instance" "vpc_b_private_ec2" {
+  ami             = "ami-0ae8f15ae66fe8cda" # Replace with the appropriate AMI ID for your region
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.private-subnet-vpc_b.id # Replace with your actual subnet ID
+  key_name        = "id_ed25519"
+  vpc_security_group_ids = [aws_security_group.sg_private_subnet_vpc_b.id]
+  tags = {
+    Name = "private_ec2_vpc_b"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# create vpc peering connection between vpc-a and vpc-b
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id = aws_vpc.vpc_a.id
+  peer_vpc_id = aws_vpc.vpc_b.id
+  auto_accept = true
+}
+
+# add route table rule for vpc-b private subnet to route traffic to vpc-a via peering connection
+
